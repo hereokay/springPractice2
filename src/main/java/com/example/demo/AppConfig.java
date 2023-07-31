92a965cf34d2db025e1457453ac6c30afa20a635
hereokay <hereokay88@gmail.com>
2023-07-31 18:42:37 +0900

    AppConfig 보기좋게 리팩토링

--- src/main/java/com/example/demo/AppConfig.java
+++ src/main/java/com/example/demo/AppConfig.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.demo.discount.DiscountPolicy;
 import com.example.demo.discount.FixDiscountPolicy;
 import com.example.demo.member.MemberRepository;
 import com.example.demo.member.MemberService;
@@ -11,13 +12,20 @@ import com.example.demo.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
         return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new FixDiscountPolicy()
+                memberRepository(),
+                discountPolicy()
         );
+    }
+    public DiscountPolicy discountPolicy (){
+        return new FixDiscountPolicy();
     }
 }
